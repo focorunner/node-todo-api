1a3b2c31e27616de88ce29a477caa456d92e6a97
Mark <mark@focorunner.com>
2018-01-26 15:09:24 -0700

    Add tests for POST /users/login

--- server/server.js
+++ server/server.js
@@ -113,19 +113,17 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user.filterUserObj());
 });
 
-// POST /users/login (email, password)
 app.post('/users/login', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
-  
+
   User.findByCredentials(body.email, body.password).then((user) => {
     return user.generateAuthToken().then((token) => {
       res.header('x-auth', token).send(user);
     });
-    res.send(user);
   }).catch((e) => {
     res.status(400).send();
   });
-})
+});
 
 app.listen(port, () => {
   console.log(`Started at port ${port}`);
